f3dcaddb2773d13217086fe9069fb9ad4b5da1c0
KadiWaleed <kady.wa12@gmail.com>
2023-02-01 21:56:13 +0300

    .

--- app/src/main/java/com/zephgv/mad/beastfitness/WorkoutActivity.java
+++ app/src/main/java/com/zephgv/mad/beastfitness/WorkoutActivity.java
@@ -12,7 +12,7 @@ import android.widget.Toast;
 
 public class WorkoutActivity extends AppCompatActivity {
 
-    CardView cd1,cd2,cd3,cd4,cd5,cd6,cd7,cd8;
+    CardView cd1,cd2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,12 +22,7 @@ public class WorkoutActivity extends AppCompatActivity {
 
         cd1 = findViewById(R.id.pushcard);
         cd2 = findViewById(R.id.pullcard);
-        cd3 = findViewById(R.id.squartcard);
-        cd4 = findViewById(R.id.tplankcard);
-        cd5 = findViewById(R.id.deadbugcard);
-        cd6 = findViewById(R.id.skippingcard);
-        cd7 = findViewById(R.id.heavysqcard);
-        cd8 = findViewById(R.id.splitjumpcard);
+
 
         cd1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,54 +37,6 @@ public class WorkoutActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Toast.makeText(WorkoutActivity.this, "Opening Youtube!", Toast.LENGTH_SHORT).show();
                 gotoUri("https://www.youtube.com/watch?v=eGo4IYlbE5g");
-            }
-        });
-
-        cd3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(WorkoutActivity.this, "Opening Youtube!", Toast.LENGTH_SHORT).show();
-                gotoUri("https://www.youtube.com/watch?v=YaXPRqUwItQ");
-            }
-        });
-
-        cd4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(WorkoutActivity.this, "Opening Youtube!", Toast.LENGTH_SHORT).show();
-                gotoUri("https://www.youtube.com/watch?v=rTY5mqJ1HNo");
-            }
-        });
-
-        cd5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(WorkoutActivity.this, "Opening Youtube!", Toast.LENGTH_SHORT).show();
-                gotoUri("https://www.youtube.com/watch?v=4XLEnwUr1d8");
-            }
-        });
-
-        cd6.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(WorkoutActivity.this, "Opening Youtube!", Toast.LENGTH_SHORT).show();
-                gotoUri("https://www.youtube.com/watch?v=u3zgHI8QnqE");
-            }
-        });
-
-        cd7.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(WorkoutActivity.this, "Opening Youtube!", Toast.LENGTH_SHORT).show();
-                gotoUri("https://www.youtube.com/watch?v=Uv_DKDl7EjA");
-            }
-        });
-
-        cd8.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(WorkoutActivity.this, "Opening Youtube!", Toast.LENGTH_SHORT).show();
-                gotoUri("https://www.youtube.com/watch?v=qsF1gYTWTrQ");
             }
         });
 
